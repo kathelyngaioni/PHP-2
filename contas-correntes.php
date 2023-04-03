9d3424caf518ff1d2cbf4943c4830caaea237a83
Kathelyn Gaioni <112026988+kathelyngaioni@users.noreply.github.com>
2023-04-03 18:42:27 +0000

    arrays-associativos

---
+++ contas-correntes.php
@@ -0,0 +1,21 @@
+<?php
+
+
+//arrays associativos em PHP
+$conta1 = [
+  'titular'=> 'Kathelyn', 
+  'saldo'=> 1000 
+];
+
+$conta2 = [
+  'titular' => 'João',
+  'saldo'=> 5000
+];
+
+echo $conta1['titular'] . PHP_EOL;
+$contascorrentes = [$conta1, $conta2];
+
+for($i=0; $i<count($contascorrentes); $i++){
+  echo $contascorrentes[$i]['titular'] . PHP_EOL;
+}
+?>
