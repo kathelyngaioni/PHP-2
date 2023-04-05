9ead9f290097740369cb2e3d742bbdd0e6afa296
Kathelyn Gaioni <112026988+kathelyngaioni@users.noreply.github.com>
2023-04-05 13:19:31 +0000

    function em PHP

--- banco.php
+++ banco.php
@@ -15,17 +15,35 @@ $contascorrentes = [
   ]
 ];
 
-function saque(string $cpf, float $valor) :void
+//função sacar
+function saque($cpf, float $valor)
 {
-  if($valor > $contascorrentes[$cpf]['saldo'])
-    echo "Saldo insuficiente" . PHP_EOL;
+  if($valor > $cpf['saldo'])
+    echo "Saldo insuficiente" . PHP_EOL;  
   else
-    $contascorrentes[$cpf]['saldo'] -= $valor;
+  $cpf['saldo'] -= $valor;
+
+  return $cpf['saldo'];
 }
 
-//operação de saque.
-saque("147.258.369-12",500);
-//$contascorrentes['147.258.369-12']['saldo'] -= 500;
+//função depositar
+function deposito($cpf, float $valor)
+{
+  if($valor > 0)
+    $cpf['saldo'] += $valor;
+  else
+    echo "Valor inválido" .  PHP_EOL;
+  return $cpf['saldo'];
+}
+
+//operação de saque
+$contascorrentes['147.258.369-12']['saldo'] = saque($contascorrentes['147.258.369-12'],500);
+
+$contascorrentes['123.654.789-72']['saldo'] = saque($contascorrentes['123.654.789-72'], 6000);
+
+//operação de deposito
+$contascorrentes['123.456.789-10']['saldo'] = deposito($contascorrentes['123.456.789-10'],200);
+
 
 foreach($contascorrentes as $cpf => $conta){
   echo $cpf . " " . $conta['titular'] . " " . $conta['saldo'] . PHP_EOL;
